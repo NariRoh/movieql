35d3eaf615ef995069180e9e356bb495e0acefea
Nari Roh <nari.rohh@gmail.com>
2018-09-20 22:59:36 -0500

    Add movie schema

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,19 +1,31 @@
-import { people, getById } from './db';
-
-const nicolas = {
-    name: "Nicolas",
-    age: 18,
-    gender: "female"
-};
+import { getMovies, getById } from "./db";
 
 const resolvers = {
     Query: {
-        people: () => people,
-        person: (_, { id }) => getById(id)
-        // person: () => nicolas
-        // when query is name ..
-        // name: () => 'nicolas'
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
     }
-};
+}
+
+// import { people, getById } from './db';
+
+// const nicolas = {
+//     name: "Nicolas",
+//     age: 18,
+//     gender: "female"
+// };
+
+// const resolvers = {
+//     Query: {
+//         people: () => people,
+//         person: (_, { id }) => getById(id),
+//         // person: (_, args) => {
+//         //     console.log(args);
+//         // }
+//         // person: () => nicolas
+//         // when query is name ..
+//         // name: () => 'nicolas'
+//     }
+// };
 
 export default resolvers;
